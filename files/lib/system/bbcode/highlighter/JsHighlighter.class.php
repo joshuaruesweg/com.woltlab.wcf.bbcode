95c1a41d89d6a22e4586d1bdaeb151f644dde8de
Tim Düsterhus <timwolla@arcor.de>
2011-12-01 21:00:01 +0100

    Adding JS-Highlighter

---
+++ files/lib/system/bbcode/highlighter/JsHighlighter.class.php
@@ -0,0 +1,119 @@
+<?php
+namespace wcf\system\bbcode\highlighter;
+
+/**
+ * Does JavaScript-highlighting.
+ * 
+ * @author	Tim Düsterhus, Michael Schaefer
+ * @copyright	2001-2011 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf.bbcode
+ * @subpackage	system.bbcode.highlighter
+ * @category 	Community Framework
+ */
+class JsHighlighter extends Highlighter {
+	// highlighter syntax
+	protected $separators = array("(", ")", "{", "}", "[", "]", ";", ".", ",");
+	protected $operators = array("=", ">", "<", "!", "~", "?", ":", "==", "<=", ">=", "!=",
+		"&&", "||", "++", "--", "+", "-", "*", "/", "&", "|", "^", "%", "<<", ">>", ">>>", "+=", "-=", "*=",
+		"/=", "&=", "|=", "^=", "%=", "<<=", ">>=", ">>>=");
+	
+	protected $keywords1 = array(
+		"String",
+		"Array",
+		"RegExp",
+		"Function",
+		"Math",
+		"Number",
+		"Date",
+		"Image",
+		"window",
+		"document",
+		"navigator",
+		"onAbort",
+		"onBlur",
+		"onChange",
+		"onClick",
+		"onDblClick",
+		"onDragDrop",
+		"onError",
+		"onFocus",
+		"onKeyDown",
+		"onKeyPress",
+		"onKeyUp",
+		"onLoad",
+		"onMouseDown",
+		"onMouseOver",
+		"onMouseOut",
+		"onMouseMove",
+		"onMouseUp",
+		"onMove",
+		"onReset",
+		"onResize",
+		"onSelect",
+		"onSubmit",
+		"onUnload"
+	);
+	protected $keywords2 = array(
+		"break",
+		"continue",
+		"do",
+		"while",
+		"export",
+		"for",
+		"in",
+		"if",
+		"else",
+		"import",
+		"return",
+		"label",
+		"switch",
+		"case",
+		"var",
+		"with",
+		"delete",
+		"new",
+		"this",
+		"typeof",
+		"void",
+		"abstract",
+		"boolean",
+		"byte",
+		"catch",
+		"char",
+		"class",
+		"const",
+		"debugger",
+		"default",
+		"double",
+		"enum",
+		"extends",
+		"false",
+		"final",
+		"finally",
+		"float",
+		"function",
+		"implements",
+		"goto",
+		"instanceof",
+		"int",
+		"interface",
+		"long",
+		"native",
+		"null",
+		"package",
+		"private",
+		"protected",
+		"public",
+		"short",
+		"static",
+		"super",
+		"synchronized",
+		"throw",
+		"throws",
+		"transient",
+		"true",
+		"try",
+		"volatile"
+	);
+}
