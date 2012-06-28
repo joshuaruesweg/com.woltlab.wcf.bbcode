44460710af49aa33c73b9a60a855ce60bf665464
Alexander Ebert <ebert@woltlab.com>
2012-06-29 01:00:49 +0200

    Added action to load smilies on runtime

--- files/lib/data/smiley/category/SmileyCategoryAction.class.php
+++ files/lib/data/smiley/category/SmileyCategoryAction.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace wcf\data\smiley\category;
 use wcf\data\AbstractDatabaseObjectAction;
+use wcf\system\exception\UserInputException;
+use wcf\system\WCF;
 
 /**
  * Executes smiley category-related actions.
@@ -17,4 +19,46 @@ class SmileyCategoryAction extends AbstractDatabaseObjectAction {
 	 * @see	wcf\data\AbstractDatabaseObjectAction::$className
 	 */
 	protected $className = 'wcf\data\smiley\category\SmileyCategoryEditor';
+	
+	/**
+	 * @see	wcf\data\AbstractDatabaseObjectAction::$allowGuestAccess
+	 */
+	protected $allowGuestAccess = array('getSmilies');
+	
+	/**
+	 * active smiley category
+	 * @var	wcf\data\smiley\category\SmileyCategory
+	 */
+	public $smileyCategory = null;
+	
+	/**
+	 * Validates smiley category id.
+	 */
+	public function validateGetSmilies() {
+		$this->readObjects();
+		
+		if (count($this->objects) != 1) {
+			throw new UserInputException('objectID');
+		}
+		
+		$this->smileyCategory = reset($this->objects);
+	}
+	
+	/**
+	 * Returns parsed template for smiley category's smilies.
+	 * 
+	 * @return	array
+	 */
+	public function getSmilies() {
+		$this->smileyCategory->loadSmilies();
+		
+		WCF::getTPL()->assign(array(
+			'smilies' => $this->smileyCategory
+		));
+		
+		return array(
+			'smilieyCategoryID' => $this->smileyCategory->smileyCategoryID,
+			'template' => WCF::getTPL()->fetch('__messageFormSmilies')
+		);
+	}
 }
