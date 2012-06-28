18f5984f8f62264cac655d9598b0167c8a48f1b8
Alexander Ebert <ebert@woltlab.com>
2012-06-28 17:28:21 +0200

    Smiley categories now carry their associated smilies

--- files/lib/data/smiley/category/SmileyCategory.class.php
+++ files/lib/data/smiley/category/SmileyCategory.class.php
@@ -1,6 +1,9 @@
 <?php
 namespace wcf\data\smiley\category;
+use wcf\data\smiley\SmileyCache;
 use wcf\data\DatabaseObject;
+use wcf\data\ITraversableObject;
+use wcf\system\exception\SystemException;
 
 /**
  * Represents a smiley category.
@@ -12,7 +15,7 @@ use wcf\data\DatabaseObject;
  * @subpackage	data.smiley.category
  * @category 	Community Framework
  */
-class SmileyCategory extends DatabaseObject {
+class SmileyCategory extends DatabaseObject implements \Countable, ITraversableObject {
 	/**
 	 * @see	wcf\data\DatabaseObject::$databaseTableName
 	 */
@@ -22,4 +25,113 @@ class SmileyCategory extends DatabaseObject {
 	 * @see	wcf\data\DatabaseObject::$databaseTableIndexName
 	 */
 	protected static $databaseTableIndexName = 'smileyCategoryID';
+	
+	/**
+	 * current iterator index
+	 * @var	integer
+	 */
+	protected $index = 0;
+	
+	/**
+	 * list of index to object relation
+	 * @var	array<integer>
+	 */
+	protected $indexToObject = null;
+	
+	/**
+	 * list of assigned smilies
+	 * @var	array<wcf\data\smiley\Smiley>
+	 */
+	public $smilies = null;
+	
+	/**
+	 * Loads associated smilies from cache.
+	 */
+	public function loadSmilies() {
+		if ($this->smilies === null) {
+			$this->smilies = SmileyCache::getInstance()->getCategorySmilies($this->smileyCategoryID);
+			$this->indexToObject = array_keys($this->smilies);
+		}
+	}
+	
+	/**
+	 * @see	\Countable::count()
+	 */
+	public function count() {
+		return count($this->smilies);
+	}
+	
+	/**
+	 * @see	\Iterator::current()
+	 */
+	public function current() {
+		$objectID = $this->indexToObject[$this->index];
+		return $this->smilies[$objectID];
+	}
+	
+	/**
+	 * CAUTION: This methods does not return the current iterator index,
+	 * rather than the object key which maps to that index.
+	 *
+	 * @see	\Iterator::key()
+	 */
+	public function key() {
+		return $this->indexToObject[$this->index];
+	}
+	
+	/**
+	 * @see	\Iterator::next()
+	 */
+	public function next() {
+		++$this->index;
+	}
+	
+	/**
+	 * @see	\Iterator::rewind()
+	 */
+	public function rewind() {
+		$this->index = 0;
+	}
+	
+	/**
+	 * @see	\Iterator::valid()
+	 */
+	public function valid() {
+		return isset($this->indexToObject[$this->index]);
+	}
+	
+	/**
+	 * @see	\SeekableIterator::seek()
+	 */
+	public function seek($index) {
+		$this->index = $index;
+	
+		if (!$this->valid()) {
+			throw new \OutOfBoundsException();
+		}
+	}
+	
+	/**
+	 * @see	wcf\data\ITraversableObject::seekTo()
+	 */
+	public function seekTo($objectID) {
+		$this->index = array_search($objectID, $this->indexToObject);
+	
+		if ($this->index === false) {
+			throw new SystemException("object id '".$objectID."' is invalid");
+		}
+	}
+	
+	/**
+	 * @see	wcf\data\ITraversableObject::search()
+	 */
+	public function search($objectID) {
+		try {
+			$this->seekTo($objectID);
+			return $this->current();
+		}
+		catch (SystemException $e) {
+			return null;
+		}
+	}
 }
